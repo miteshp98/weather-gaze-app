8ed5fbbcf944a9c2049ca174f1a03af17277e807
miteshp98 <miteshpanchal3d@gmail.com>
2024-10-07 13:47:18 +0530

    initiate  recreate  branch

--- js/config.js
+++ js/config.js
@@ -1,28 +1,5 @@
-export const URL =
-  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 export const API_KEY = "LDLZD5U6GVYWG8UCB7MTZ84ZL";
 export const UNITS = "metric";
-
-export const Dom = () => {
-  const form = document.querySelector("form");
-  const search = form.children[0];
-  const mainOverviewSection = document.querySelector(".main-overview-section");
-  const citiesSearchSection = document.querySelector(".cities-search-section");
-  const sevenDayForecastAside = document.querySelector(".sevenday-forecast");
-  const citiesAsideSection = document.querySelector(".city-aside-section");
-  const navLinks = document.querySelectorAll(".nav-link");
-  const mapSection = document.querySelector(".map-section");
-  const mapAsideSection = document.querySelector(".map-aside-section");
-
-  return {
-    form,
-    search,
-    mainOverviewSection,
-    sevenDayForecastAside,
-    citiesSearchSection,
-    citiesAsideSection,
-    mapSection,
-    mapAsideSection,
-    navLinks,
-  };
-};
+export const WEATHER_URL =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+export const TIMEOUT_SEC = 5;
---
+++ js/helper.js
@@ -0,0 +1,2 @@
+import { TIMEOUT_SEC } from "./config.js";
+import { WEATHER_URL } from "./config.js";
