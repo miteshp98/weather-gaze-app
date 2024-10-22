37df0f7cbc512128cc4b1dcb75ecf8c583cbca6f
miteshp98 <miteshpanchal3d@gmail.com>
2024-10-23 01:49:39 +0530

    fix extra indent and fix some lines

--- js/views/citiesview.js
+++ js/views/citiesview.js
@@ -9,6 +9,7 @@ class CitiesView extends WeatherDashboard {
     super();
   }
 
+  // Renders the city data in the UI
   render(data) {
     if (!data) {
       return;
@@ -19,6 +20,7 @@ class CitiesView extends WeatherDashboard {
     this.renderCitySection(currentSearchCityData);
   }
 
+  // Processes the data received and triggers relevant updates
   getData(data) {
     if (!data) {
       return;
@@ -29,6 +31,7 @@ class CitiesView extends WeatherDashboard {
     mapView._getData(weatherData);
   }
 
+  // Retrieves the user's input query from the search form
   getQuery() {
     const query = this._formElement.querySelector("#city-search-input").value;
 
@@ -37,10 +40,12 @@ class CitiesView extends WeatherDashboard {
     return query;
   }
 
+  // Clears the input field for the city search
   clearQuery() {
     this._formElement.querySelector("#city-search-input").value = "";
   }
 
+  // Adds a submit event listener to the search form
   addHandleSearch(handler) {
     this._formElement.addEventListener("submit", function (e) {
       e.preventDefault();
@@ -48,6 +53,7 @@ class CitiesView extends WeatherDashboard {
     });
   }
 
+  // Adds a click event listener to the city search results
   addHandleSearchedCity(handler) {
     const section = document.querySelector(".cities-section");
 
@@ -65,6 +71,7 @@ class CitiesView extends WeatherDashboard {
     });
   }
 
+  // Generates markup for displaying search results
   generateSearchResults(data) {
     const container = document.querySelector(".city-search-result-container");
 
@@ -74,26 +81,23 @@ class CitiesView extends WeatherDashboard {
         const locationName = address.charAt(0).toUpperCase() + address.slice(1);
         const cityCurrentTemp = currentConditions.temp;
         const cityCurrentTime = currentConditions.datetime;
+        const icon = this._weatherIconObject[currentConditions.icon];
 
         const markup = `
-        <button class="city-search-result" data-cityname="${locationName.toLowerCase()}">
-              
 
-                <img src="assets/icons/weathericon/clear-day.svg"
-                        alt="clear-day" class="weather-condition-icon"/>
-
-            <div class="city-name-time-wrapper">
-
-                <h2 class="city-name-label">${locationName}</h2>
-                <p class="city-current-time">${this.convertTo12HourFormat(
-                  cityCurrentTime
-                )}</p>
-
-            </div>
-
-              <h3 class="city-current-temperature">${Math.round(
-                cityCurrentTemp
-              )}&deg;</h3>
+           <button class="city-search-result" data-cityname="${locationName.toLowerCase()}">
+                <img src="${icon}" alt="${
+          currentConditions.icon
+        }" class="weather-condition-icon"/>
+                <div class="city-name-time-wrapper">
+                    <h2 class="city-name-label">${locationName}</h2>
+                    <p class="city-current-time">${this.convertTo12HourFormat(
+                      cityCurrentTime
+                    )}</p>
+                </div>
+                <h3 class="city-current-temperature">${Math.round(
+                  cityCurrentTemp
+                )}&deg;</h3>
         </button>`;
 
         return markup;
@@ -103,62 +107,47 @@ class CitiesView extends WeatherDashboard {
     container.innerHTML = render;
   }
 
+  // Renders the city forecast section in the UI
   renderCitySection(data) {
     const { address, currentConditions } = data;
     const locationName = address.charAt(0).toUpperCase() + address.slice(1);
+    const icon = this._weatherIconObject[currentConditions.icon];
 
     const sectionMarkup = `
 
-    <div class="city-overview-section">
-
-        <div class="city-current-forecast-container">
-
+          <div class="city-current-forecast-container">
               <div class="current-forecast-wrapper">
-
                 <div class="current-city-info-wrap">
-
                   <h2 class="city-name">${locationName}</h2>
                   <p class="rain-probability">Chance of rain ${
                     currentConditions.precipprob
                   }%</p>
-
                 </div>
-
                 <h3 class="city-temp">${Math.round(
                   currentConditions.temp
                 )}&deg;</h3>
-
                 </div>
-
-              <img
-                src="assets/icons/weathericon/clear-day.svg"
-                alt="clear-day"
-                class="current-weather-icon"
-              />
-
+              <img src="${icon}" alt="${
+      currentConditions.icon
+    }" class="current-weather-icon"/>
         </div>
-
         <div class="city-hourly-forecast-container">
               <h2 class="city-wrapper-title">today's forecast</h2>
-
             <div class="hourly-forecast-wrapper">
               ${this.generateHourlyForecast(data)}
             </div>
         </div>
-
         <div class="city-threeday-forecast-container">
               <h2 class="city-wrapper-title">3-day forecast</h2>
-
               <div class="threeday-forecast-wrapper">
-              ${this.generateThreedayForecast(data)}
+              ${this.generateThreedayForecast(data)} 
               </div>
-        </div>
-
-    </div>`;
+        </div>`;
 
     this._parentElement.innerHTML = sectionMarkup;
   }
 
+  // Generates markup for the hourly forecast
   generateHourlyForecast(data) {
     const { days } = data;
     const { hours } = days[0];
@@ -167,29 +156,24 @@ class CitiesView extends WeatherDashboard {
       .filter((_, index) => index % 2 === 0)
       .slice(0, 3)
       .map((hour, index) => {
+        const icon = this._weatherIconObject[hour.icon];
         return `
 
-            <div class="city-hourly-forecast">
-                
+       <div class="city-hourly-forecast">
                   <p class="city-current-time">${this.convertTo12HourFormat(
                     hour.datetime
                   )}</p>
-
-                  <img
-                    src="assets/icons/weathericon/clear-day.svg"
-                    alt="clear-day"
-                    class="condition-icon"
-                  />
-
+                  <img src="${icon}" alt="${hour.icon}" class="condition-icon"/>
                   <p class="city-current-temp">${Math.round(hour.temp)}&deg;</p>
             </div>
-        `;
+          `;
       })
       .join("");
 
     return markup;
   }
 
+  // Generates markup for the 3-day forecast
   generateThreedayForecast(data) {
     const { days } = data;
 
@@ -197,25 +181,20 @@ class CitiesView extends WeatherDashboard {
       .slice(0, 3)
       .map((data, index) => {
         const { conditions } = data;
+        const icon = this._weatherIconObject[data.icon];
 
         return `
 
-        <div class="forecast-day">
-                
+          <div class="forecast-day">
                     <p class="forecast-weekday">${this.getWeekDays(
                       data.datetime
                     )}</p>
-
                   <p class="forecast-condition">
-                    <img
-                      src="assets/icons/weathericon/clear-day.svg"
-                      alt="clear-day"
-                    />
+                    <img src="${icon}" alt="${data.icon}"/>
                     <span class="condition-label">${
                       conditions.split(",")[0]
-                    }</span>
+                    }</span> 
                   </p>
-
                   <p class="forecast-temperature">
                     <span class="temp-high">${Math.round(
                       data.tempmax
@@ -223,7 +202,6 @@ class CitiesView extends WeatherDashboard {
           data.tempmin
         )}</span>
                   </p>
-
         </div>`;
       })
       .join("");
